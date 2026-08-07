8b0834540409608c36410b2033697a20a187d626
samarth67 <samarthmane44@gmail.com>
2026-08-07 16:46:38 +0530

    Update favicon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,24 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between border-b border-zinc-800 bg-black px-8 py-5 text-white">
-      <Link href="/" className="text-2xl font-bold">
-        Link<span className="text-blue-500">Pay</span>
-      </Link>
+      <Link href="/" className="flex items-center gap-3">
+  <Image
+    src="/favicon.png"
+    alt="LinkPay"
+    width={38}
+    height={38}
+    priority
+  />
+
+  <span className="text-2xl font-bold">
+    Link<span className="text-blue-500">Pay</span>
+  </span>
+</Link>
 
       <div className="flex items-center gap-6">
   <Link href="/">Home</Link>
